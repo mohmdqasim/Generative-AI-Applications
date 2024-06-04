49a668ad23648248e853be75cf61ff0cef5ff77c
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-05 01:19:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from dotenv import load_dotenv
 from introduction import intro
 from llama import chat_bot_llama
 from imageGenerator import image_generator
+from streamlit_navigation_bar import st_navbar
 load_dotenv()
-
 page_names_to_funcs = {
     "Home Page": intro,
     "Chatbot with Llama": chat_bot_llama,
     "Image Generator": image_generator,
     # "DataFrame Demo": data_frame_demo
 }
-
-demo_name = st.sidebar.selectbox("Choose an Application", page_names_to_funcs.keys())
-page_names_to_funcs[demo_name]()
+page = st_navbar(list(page_names_to_funcs.keys()))
+page_names_to_funcs[page]()
