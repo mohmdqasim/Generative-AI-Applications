891a666aaea1434d240592109a3f9c2771af65aa
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-05 01:35:15 +0500

    Update imageGenerator.py

--- imageGenerator.py
+++ imageGenerator.py
@@ -4,7 +4,10 @@ import replicate
 def image_generator():
     st.title("Image Generator")
     prompt = st.text_input("Enter your prompt Here..")
-    if prompt:
+    button = st.button("Generate!")
+    if not prompt:
+        st.info("Please enter your prompt!")
+    if prompt and button:
         with st.spinner('Wait for it...'):
             output = replicate.run(
                 "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
